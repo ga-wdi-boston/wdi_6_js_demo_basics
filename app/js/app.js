d621a5e73541e93f763fdf827300e8793dc7b3c7
KBondanza <kelly.bondanza@gmail.com>
2015-02-13 14:52:12 -0500

    did in class

--- app/js/app.js
+++ app/js/app.js
@@ -4,34 +4,50 @@
 ———————————————————————————————————————————————————
 Unlike in Ruby there is no special distinction between numbers with and
 without decimal points. They are all just "numbers".  */
+var currentLevel = 17;
+var price = 1499.99;
+var fiveMinutes = 60 * 5;
+var threeHalves = 3 / 2;
+price = 1299.99;
+price += 100;
 
+var infinity = 42 / +0;
+var negativeInfinity = 42 / -0;
 
 /* STRINGS
 ———————————————————————————————————————————————————
 Unlike in Ruby there is no difference between single quotes and double
 quotes... since JavaScript has no string interpolation. Strings are also
 "immutable", meaning we can't modify them in-place (no shoveling!)  */
-
+var greeting = 'Hello there!';
+var firstName = "Jason";
+var lastName = "Wharrf";
+var myName = firstName + ' ' + lastName;
 
 /*  BOOLEANS
 ———————————————————————————————————————————————————
 Like in Ruby, we have the booleans true and false. */
+var excited = true;
+var testMode = false;
 
+var excitedlyTesting = excited && testMode;
+var calm = !excited;
 
 /* UNDEFINED
 ———————————————————————————————————————————————————
 Unlike in Ruby, we have an extra nil-like value called "undefined". It's
 what you'll get if you access a variable that's not assigned yet, or call
 a function that doesn't return anything.*/
-
-
+var mystery = undefined;
+var spooky;
 
 /*  NULL
 ———————————————————————————————————————————————————
 We also have nil, but in JS it's called "null". A value that carries no value.
 Like undefined, but defined (not automatically assigned).
 It is falsy.  */
-
+var result = null;
+console.log(typeof undefined);
 
 
 /*  Control Flow in Javascript
@@ -41,9 +57,24 @@ Important differences from Ruby:
   - Code blocks are always enclosed in braces. There is no `end` in JavaScript.
   - The "else-if" syntax is two separate words, `else if`, rather than `elsif`.
   - There is no `unless` in JavaScript. Use the "not" operator (`!`) instead.   */
+var holyNumber = function(holyNumber) {
+  if(holyNumber > 3) {
+    return 'Four shalt thou not count. Five is right out.';
+  } else if(holyNumber < 3) {
+    return 'Count neither one nor two, expecting that thou then proceedest to three.';
+  } else if(holyNumber === 3) {
+    return 'Throw the holy hand grenade!';
+  } else {
+    return 'World ends';
+  }
+};
 
-
-
+holyNumber('4');
+holyNumber(4);
+holyNumber('2');
+holyNumber(2);
+holyNumber('3');
+holyNumber(3);
 /*  Switch/Case statements
 ———————————————————————————————————————————————————
 Note that `case` blocks are *not* enclosed in braces,
@@ -53,8 +84,41 @@ block and keep on going! Thankfully `switch` uses the
 threequals for comparison, but due to its quirks and
 inflexibility, you don't see it that often in real-world programs.
 */
+var yearbook = function(year){
+  switch(year) {
+    case 'freshman':
+      return 'cannon fodder';
+      break;
+    case 'sophomore':
+      return 'mildly respectable';
+      break;
+    case 'junior':
+      return 'some influence';
+      break;
+    case 'senior':
+      return 'phenomenal cosmic power';
+      break;
+    default:
+      return 'mysterious stanger'
+      break;
+  }
+};
 
+3 == '3'
+0 == false
+1 == true
+2 != true
+2 != false
+0 == ''
+0 == []
+'' == []
+'wat' == ['wat']
+null == undefined
 
+3 == '3'
+1 != '1'
+3 === '3'
+1 !== '1'
 
 /*  Arrays
 ———————————————————————————————————————————————————
@@ -65,8 +129,19 @@ passing any arguments. Note `length` is not a function &ndash;
 instead it is a *property* that is accessed directly, and we
 cannot use parentheses to call it. The MDN reference will
 tell you whether something is a property or a function. */
+var colors = ['red', 'green', 'blue'];
+var green = colors[1];
+var colorsCount = colors.length;
+var indexOfBlue = colors.indexOf('blue');
+var lastColor = colors[colors.length - 1];
 
+colors.push('purple');
+var purple = colors.pop();
 
+colors.sort();
+
+var newColors = 'blue, orange, yellow'.split(', ');
+var joinedColors = newColors.join(' and ');
 
 /*  Iterating through arrays
 ———————————————————————————————————————————————————
@@ -88,7 +163,15 @@ defining an anonymous function, which we'll get into later.
 There are also `map` and `reduce` functions that do the same thing
 as their Ruby equivalents.
 */
+var colors = ['red', 'green', 'blue'];
+var tmpColors = [];
+for(var i = 0; i < colors.length; i++) { tmpColors.push(colors[i] + ' is one of my favorite colors');
+}
 
+var tempColors = [];
+colors.forEach(function(color, index, array) {
+    tempColors.push(color + ' is favorite color number ' + index);
+  });
 
 /*  JS Objects
 ———————————————————————————————————————————————————
